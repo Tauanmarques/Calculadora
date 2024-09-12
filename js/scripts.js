fedbbd22b67c84791a40ef1f842632816560322a
Tauan <tauanfrancisco45@gmail.com>
2024-09-12 20:28:42 -0300

    aula 12/09

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,34 @@
 $(document).ready(function() {
     
-    $ ('.bt-nr').click(function() {
-        varbt_nr = $(this).text();
+    var bt_nr = '';
 
-        $('txt_display').val(bt_nr);
+    $ ('.bt-nr').click(function() {
+        var n = $(this).text();
+
+        if(bt_nr[0] !== '0') {
+
+            bt_nr = bt_nr.concat(n);
+
+        } else if (bt_nr[0] !== '0' && bt_nr[1] !== '.') {
+
+            bt_nr = bt_nr.concat(n);
+
+        } else {
+
+            if(bt_nr.length == 1 && n == '.'){
+
+                bt_nr = bt_nr.concat(n);
+                $(this).attr('disabled', 'true');
+
+            } else {
+
+                bt_nr = bt_nr.concat(n);
+
+            }
+
+        }  
+       
+        $('#txt_display').val(bt_nr);
     });
-})
+
+});
